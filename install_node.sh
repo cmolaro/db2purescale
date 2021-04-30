7a49dbedf75c4d62fbf776ef5c6d7d61f990d17f
Cristian Molaro <cristian@molaro.be>
2021-04-30 14:24:38 +0200

    Update install_node.sh

--- install_node.sh
+++ install_node.sh
@@ -25,3 +25,32 @@ chmod 777 /work
 yum -y install git
 cd /work
 git clone https://github.com/cmolaro/db2purescale
+
+#get software
+mkdir /work/db2install
+chmod 777 /work/db2install
+
+#----------------------------------------------------
+# move db2 installers to /work/db2install/db211.5.5.1
+#----------------------------------------------------
+
+#packages needed for db2 11.5.5.1 in RHEL 7.7
+/work/db2install/db211.5.5.1/db2prereqcheck | less 
+
+
+yum install libstdc++.i686 # 32-bit library file: "libstdc++.so.6"
+yum install pam.i686 # 32-bit library file: "/lib/libpam.so*"
+yum install m4 # m4
+yum install gcc # DBT3507E  The db2prereqcheck utility failed to find the following package or file: "gcc"
+yum install cpp # DBT3507E  The db2prereqcheck utility failed to find the following package or file: "cpp"
+yum install gcc-c++ # DBT3507E  The db2prereqcheck utility failed to find the following package or file: "gcc-c++"
+yum install kernel-devel # DBT3507E  The db2prereqcheck utility failed to find the following package or file: "kernel-devel"
+yum install patch # DBT3507E  The db2prereqcheck utility failed to find the following package or file: "patch"
+yum install ksh # Required minimum version for "ksh": "20100621" - WARNING : Requirement not matched
+yum install ntpd # Required minimum version for "ntpd": "4.2.6p5" - WARNING : Requirement not matched
+
+# Fix for:
+# DBT3563E  The db2prereqcheck utility determined that SELinux is enabled, which is not supported with GPFS
+# Edit /etc/selinux/config to SELINUX=disabled; reboot
+
+
