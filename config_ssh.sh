623948b3100335e9bd725be102cc412438fea48b
Cristian Molaro <cristian@molaro.be>
2021-05-07 11:58:31 +0200

    Update config_ssh.sh

--- config_ssh.sh
+++ config_ssh.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# in all nodes:
+
+ssh-keygen -t rsa
+# (press enter for all questions)
+cp /root/.ssh/id_rsa.pub /root/id_rsa.pub.$(hostname)
+
+
+# in all nodes, consolidate pub keys to db2node1
+scp /root/id_rsa.pub.* root@dbnod1:/root/
+
 # ssh settings
 while true; do
     read -p "Confige Password-less SSH? (y/n)" yn
@@ -12,15 +22,15 @@ while true; do
                 echo "Run in db2nod1.tmb.com";
                 for file in /root/id_rsa.*; do cat $file >> /root/.ssh/authorized_keys; done
                 echo "Copy to all members:";
-                scp /root/.ssh/authorized_keys root@db2nod1.tmb.com:/root/.ssh/ ;
-                scp /root/.ssh/authorized_keys root@db2nod2.tmb.com:/root/.ssh/ ;
-                scp /root/.ssh/authorized_keys root@db2nod3.tmb.com:/root/.ssh/ ;
-                scp /root/.ssh/authorized_keys root@db2nod4.tmb.com:/root/.ssh/ ;
+                scp /root/.ssh/authorized_keys root@dbnod1.tmb.com:/root/.ssh/ ;
+                scp /root/.ssh/authorized_keys root@dbnod2.tmb.com:/root/.ssh/ ;
+                scp /root/.ssh/authorized_keys root@dbnod3.tmb.com:/root/.ssh/ ;
+                scp /root/.ssh/authorized_keys root@cfnod1.tmb.com:/root/.ssh/ ;
                 echo "Validate:";
-                ssh db2nod1.tmb.com ;
-                ssh db2nod2.tmb.com ;
-                ssh db2nod3.tmb.com ;
-                ssh db2nod4.tmb.com ;
+                ssh dbnod1.tmb.com ;
+                ssh dbnod2.tmb.com ;
+                ssh dbnod3.tmb.com ;
+                ssh cfnod1.tmb.com ;
                 break;;
         [Nn]* ) echo "Bye now."; exit;;
         * ) echo "Please answer yes or no.";;
